8a82b1f0529a59d5b469b11ed0fba443390c5ffc
Sajid Ansari <sajidansari33272@gmail.com>
2020-09-22 23:20:30 +0545

    ResetPassword URL modified

--- controller/auth.js
+++ controller/auth.js
@@ -75,7 +75,7 @@ const forgotPassword = asyncHandler(async (req, res, next) => {
   try {
     const resetUrl = `${req.protocol}://${req.get(
       "host"
-    )}/api/v1/auth/resetPassword/${resetToken}`;
+    )}/api/v1/auth/resetPassword/?token=${resetToken}`;
 
     const message = `You are receiving this email because you (or someone else ) has
     requested the reset of a password.`;
