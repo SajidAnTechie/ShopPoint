afbf2a6dc1bf1133282e42877b47e84e3336bf7c
Sajid Ansari <sajidansari33272@gmail.com>
2020-09-24 22:38:54 +0545

    Category route added

---
+++ routes/category.js
@@ -0,0 +1,29 @@
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controller/user");
+
+//Invoked middleware.
+const advanceResults = require("../middleware/advanceResults");
+const { protect, permission } = require("../middleware/auth");
+
+//User model
+const Category = require("../models/Category");
+
+const router = require("express").Router();
+
+router
+  .route("/")
+  .get(advanceResults(Category), getCategories)
+  .post(protect, permission("admin"), addCategory);
+
+router
+  .route("/:categoryId")
+  .get(getCategory)
+  .put(protect, permission("admin"), updateCategory)
+  .delete(protect, permission("admin"), removeCategory);
+
+module.exports = router;
