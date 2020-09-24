e3635504567727857c474108338b99c4ec06a636
Sajid Ansari <sajidansari33272@gmail.com>
2020-09-24 22:58:03 +0545

    Category file added to seeder

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/config.env" });
 //load models
 const User = require("./models/User");
 const Product = require("./models/Product");
+const Category = require("./models/Category");
 
 // Connect to DB
 
@@ -28,6 +29,9 @@ const users = JSON.parse(
 const products = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/product.json`, "utf-8")
 );
+const categories = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/category.json`, "utf-8")
+);
 
 //Import into DB
 
@@ -35,6 +39,7 @@ const importData = async () => {
   try {
     await User.create(users);
     await Product.create(products);
+    await Category.create(categories);
     console.log(`Data Imported`.green.inverse);
     process.exit();
   } catch (error) {
@@ -46,6 +51,7 @@ const deleteData = async () => {
   try {
     await User.deleteMany();
     await Product.deleteMany();
+    await Category.deleteMany();
     console.log("Data Destroy".red.inverse);
     process.exit();
   } catch (error) {
