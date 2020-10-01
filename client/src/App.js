05c55f376e3abb2764a7bf2672da4efd957d1fd3
Sajid Ansari <sajidansari33272@gmail.com>
2020-10-01 23:31:03 +0545

    Boostrap css file imported

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Hello world</h4>
     </div>
   );
 }
