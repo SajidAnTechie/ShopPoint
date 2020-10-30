6b4b2c461aeae04e339839011a9baf6b4087a39f
Sajid Ansari <sajidansari33272@gmail.com>
2020-10-30 10:32:12 +0545

    Build file added

---
+++ client/build/precache-manifest.78b0a338c7ed5ba49e0f1df923efd76c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f0b333f0d628cff260ba2c1688f349",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efb21beaf18169cc02c6",
+    "url": "/static/css/2.9151ed95.chunk.css"
+  },
+  {
+    "revision": "34afa44cf405003b2b10",
+    "url": "/static/css/main.155c28c1.chunk.css"
+  },
+  {
+    "revision": "efb21beaf18169cc02c6",
+    "url": "/static/js/2.1ecf364b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.1ecf364b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34afa44cf405003b2b10",
+    "url": "/static/js/main.390ff591.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "31a737c539bf89313cd1b4d5f43883fd",
+    "url": "/static/media/confirmation.31a737c5.png"
+  }
+]);
