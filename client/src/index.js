5517bcd794442a399ba6432934873ea9097db684
Sajid Ansari <sajidansari33272@gmail.com>
2020-10-30 09:53:41 +0545

    make it better

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import store from "./store/store";
+import store from "./store";
 import { Provider } from "react-redux";
 import "./bootstrap.min.css";
 import "./index.css";
--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ import {
   deleteProduct,
   createProduct,
   EditProduct,
-} from "../reducers/productReducer";
+} from "./reducers/productReducer";
 
 import {
   userLogin,
@@ -19,8 +19,8 @@ import {
   getUser,
   forgotPassword,
   resetPassword,
-} from "../reducers/userReducer";
-import { cartReducer } from "../reducers/cartReducer";
+} from "./reducers/userReducer";
+import { cartReducer } from "./reducers/cartReducer";
 import {
   createOrderReducer,
   getOrder,
@@ -28,7 +28,7 @@ import {
   orderDeliverReducer,
   authOrders,
   listOrders,
-} from "../reducers/orderReducer";
+} from "./reducers/orderReducer";
 
 const rootReducer = combineReducers({
   productList: listProducts,
