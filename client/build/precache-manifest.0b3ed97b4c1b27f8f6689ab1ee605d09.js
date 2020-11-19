2533eb1f8e418168d756967f89cc459703f171e4
Sajid Ansari <sajidansari33272@gmail.com>
2020-11-19 22:16:33 +0545

    new version deploy to heruku

---
+++ client/build/precache-manifest.0b3ed97b4c1b27f8f6689ab1ee605d09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7438e2b53533e0c0273d5c72425be0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4ebc0f562f1e8442eb5",
+    "url": "/static/css/2.9151ed95.chunk.css"
+  },
+  {
+    "revision": "535f5f805a40eb400eba",
+    "url": "/static/css/main.12a1d3af.chunk.css"
+  },
+  {
+    "revision": "b4ebc0f562f1e8442eb5",
+    "url": "/static/js/2.cafd108d.chunk.js"
+  },
+  {
+    "revision": "5795cac972af069c6d9a600320c75127",
+    "url": "/static/js/2.cafd108d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535f5f805a40eb400eba",
+    "url": "/static/js/main.857554d2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "31a737c539bf89313cd1b4d5f43883fd",
+    "url": "/static/media/confirmation.31a737c5.png"
+  }
+]);
